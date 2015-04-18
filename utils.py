cb7c714477a16e3e213d125f73f3732d0b004dbb
mihaiolteanu <mihai.olteanu@fastmail.fm>
2015-04-18 13:49:35 +0300

    Clear utils.py, as some things are no longer needed

--- utils.py
+++ utils.py
@@ -1,49 +1,19 @@
-#import urllib.request
+import urllib
 import datetime
 import os
 
 # Retrieve html page from web.
-# def html_retrieve(address):
-#     # Check if address is an url.
-#     #parts = urlparse(address)
-#     #if parts.netloc:
-#         return urllib.request.urlopen(address).read()
-
-
-# Path to html local samples taken from tursib.ro
-samples_path = os.path.join(os.path.dirname(__file__), r"samples")
-
-
-def html_retrieve(address):
-    """
-    Return local previously saved files instead of returning them from web.
-    :param address: tursib real address
-    :return: local html file
-    """
-    if address == "www.tursib.ro":
-        return open(os.path.join(samples_path, "tursib_ro.htm"))
-
-    if address == "www.tursib.ro/trasee":
-        return open(os.path.join(samples_path, "tursib_ro_trasee_minimal.htm"))
-
-    # Requests of the form: http://tursib.ro/traseu/11/program?statie=1&dir=dus
-    if "tursib.ro/traseu/" in address and "program?statie=" in address:
-        return open(os.path.join(samples_path, "TRASEUL 11 - SC CONTINENTAL.htm"))
-
-    # Requests of the form: http://tursib.ro/traseu/11/ (notice the similarity with the previous case).
-    if "tursib.ro/traseu/" in address:
-        return open(os.path.join(samples_path, "TRASEUL 11.htm"))
-
-    if "http://tursib.ro/news/show/" in address:
-        return open(os.path.join(samples_path, "news_160.htm"))
-
+base = "http://www.tursib.ro"
+def htmlget(address):
+    absolute = urllib.parse.urljoin(base, address)
+    return urllib.request.urlopen(absolute).read()
 
 def remove_non_ascii(s):
     return "".join(i for i in s if ord(i)<126 and ord(i)>31)
 
 # Months names in Romanian
 months_name_ro = {
-    'ianuarie': 1,
+    'ianuarie': 1, # January
     'februarie': 2,
     'martie': 3,
     'aprilie': 4,
@@ -54,5 +24,5 @@ months_name_ro = {
     'septembrie': 9,
     'octombrie': 10,
     'noiembrie': 11,
-    'decembrie': 12
+    'decembrie': 12 # December
 }
