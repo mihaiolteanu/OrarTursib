775c43f8ca38213874d9120eda078ca061d158be
mihaiolteanu <mihai.olteanu@fastmail.fm>
2015-04-18 16:42:25 +0300

    Use the requests library instead of urllib for fetching html pages

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
-import urllib
+import urllib.parse
+import requests
 import datetime
 import os
 
 # Retrieve html page from web.
 base = "http://www.tursib.ro"
 def htmlget(address):
-    absolute = urllib.parse.urljoin(base, address)
-    return urllib.request.urlopen(absolute).read()
+    path = urllib.parse.urljoin(base, address)
+    resp = requests.get(path)
+    return resp.text
 
 def remove_non_ascii(s):
     return "".join(i for i in s if ord(i)<126 and ord(i)>31)
