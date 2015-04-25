a326600052a4a8d0dbdbe40dbb8340d4a8989d81
mihaiolteanu <mihai.olteanu@fastmail.fm>
2015-04-25 13:22:29 +0300

    Try to change the root widget in the next commits
    I can't set the contents on the default tab when the application is started.

--- client/main.py
+++ client/main.py
@@ -10,29 +10,48 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.modalview import ModalView
 from kivy.lang import Builder
 from kivy.factory import Factory
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, ListProperty
 
 class TsbApp(App):
     pass
 
 class BusButton(ListItemButton):
-    def list_bus_stations(self, instance):
-        print("list bus stations " + instance.text)
+    pass
 
 class StationButton(ListItemButton):
     pass
 
 class MainMenu(TabbedPanel):
     bus_tab_content = ObjectProperty()
+    bus_number = ""
 
-    def show_the_buses(self, instance):
+    def __init__(self, **kwargs):
+        super(MainMenu, self).__init__(**kwargs)
+        self.show_the_buses()
+        print("initialized")
+
+
+    def show_the_buses(self):
+        print("press the buses tab")
+        bus_list = Factory.BusList()
+        self._replace_with(bus_list, ["bus 1", "bus 2", "bus 3", "bus 4", "bus 5"])
+
+    def show_the_stations(self, instance):
         self.clear_widgets()
         stations_list = Factory.StationsList()
+        stations_list.bus_number_name = instance.text
+        self.bus_number = instance.text
+        self._replace_with(stations_list, ["station 1", "station 2", "station 3"])
+
+    def show_the_timetable(self, instance):
+        print(self.bus_number + " " + instance.text)
+
+    def _replace_with(self, widget, data):
         # Python2.7 workaround; see page 39 from "Creating Apps in Kivy"
-        del stations_list.lst.adapter.data[:]
-        stations_list.lst.adapter.data.extend(["station 1", "station 2", "station 3"])
-        stations_list.lst._trigger_reset_populate()
-        self.add_widget(stations_list)
+        del widget.lst.adapter.data[:]
+        widget.lst.adapter.data.extend(data)
+        widget.lst._trigger_reset_populate()
+        self.add_widget(widget)
 
 if __name__ == '__main__':
     TsbApp().run()
